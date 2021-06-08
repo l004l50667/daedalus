08dd26355a7aadd65f6ebbcc94b5e368bc2c098f
l004l50667 <weixing9954agf@126.com>
2021-06-08 20:13:46 +0800

    search list

--- ddutils/views.py
+++ ddutils/views.py
@@ -2,6 +2,7 @@
 
 from django.views.generic import ListView
 from django.shortcuts import render
+from django.http.response import JsonResponse
 
 from .utils import DDPagination
 from .mixins import LoginRequiredMixin
@@ -12,18 +13,36 @@ class DDListView(ListView, LoginRequiredMixin):
     queryset = None
     model = None
     pagination_class = DDPagination
+    serializer_fields = ()
 
     def get_messages(self):
         return {}
 
     def get(self, request, *args, **kwargs):
-        pagination = self.pagination_class(request, self.queryset).pagination()
+        pagination = self.pagination_class(request, self.get_queryset(request)).pagination()
         message = self.get_messages()
         context = self.get_context_data(request, *args, **kwargs)
         context.update(message)
         context.update(pagination)
-        print context
+        if request.is_ajax():
+            print 'is ajax ====================='
+            if self.serializer_fields and pagination.get('result', []):
+                print 'is ajax serializer data'
+                data = self.searializer_data(pagination)
+                return JsonResponse(data)
+
+        print 'template response'
         return render(request, self.template_name, context=context)
 
     def get_context_data(self, request, *args, **kwargs):
         return {}
+
+    def get_queryset(self, request):
+        return self.queryset
+
+    def searializer_data(self, pagination):
+        pagination['result'] = [
+            { field: getattr(obj, field) for field in self.serializer_fields}
+            for obj in pagination['result']
+        ]
+        return pagination
--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,7 @@
 # coding: utf-8
-from django.shortcuts import render
-from django.views.generic import ListView
+
+from django.db.models import Q
+from django.conf import settings
 
 from .models import Task, Category
 from ddutils.views import DDListView
@@ -10,6 +11,42 @@ class TaskListView(DDListView):
     model = Task
     queryset = Task.objects.all()
     template_name = "jobs/task_list.html"
+    serializer_fields = ('name', 'type', 'category')
+
+    def get_context_data(self, request, *args, **kwargs):
+        categories = [category for category in Category.objects.all()]
+        types = ['shell', 'playbook']
+        return {'categories': categories, 'types': types}
+
+    def searializer_data(self, pagination):
+        tasks = []
+        for task in pagination['result']:
+            category = Category.objects.get(id=task.category)
+            task.category = category.display
+            tasks.append(task)
+        pagination['result'] = tasks
+        return super(TaskListView, self).searializer_data(pagination)
+
+    def get_queryset(self, request):
+        queryset = self.queryset
+        type = request.GET.get('type', None)
+        name = request.GET.get('name', None)
+        category = request.GET.get('category', None)
+
+        if category:
+            try:
+                category = Category.objects.get(name=category)
+            except Category.DoesNotExist:
+                category = None
+            if category:
+                queryset = queryset.filter(category=category.id)
+        if type in settings.TASK_TYPES.values():
+            queryset = queryset.filter(type=type)
+
+        if name:
+            queryset = queryset.filter(Q(name__contains=name))
+
+        return queryset.all()
 
 
 class CategoryListView(DDListView):
